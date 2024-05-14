5aa154970d665936355a2a9cfd6a8a7b39ca0f2c
FranyValde <franyvalde@gmail.com>
2024-05-14 08:40:37 -0300

    commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import productsRoutes from './routes/products.routes.js'
 
-const puerto = 8100
+const puerto = 8080
 const app = express()
 app.use(express.json)
 app.use(express.urlencoded({extended:true}))
 
 
 
-app.use("/api", (productsRoutes))
+app.use("/", productsRoutes)
 
 app.listen(puerto, ()=> console.log('Server listening on ' + puerto))
 app.on("Error", (error)=> console.log("Server error "+error))
